9cc4a704528aa732d09c09234f6c22132f6ae861
Fred <fledinahet@gmail.com>
2013-07-12 17:10:05 +0000

    full web.js

--- web.js
+++ web.js
@@ -1,26 +1,21 @@
-//var express = require('express');
+var express = require('express');
 
 var fs = require('fs');
 
 buf = new Buffer(256);
 len = buf.write(fs.readFileSync('index.html','utf8'));
 
+texte = buf.toString('utf8', 0, len);
+console.log(texte);
 
 
-//console.log(fs.readFileSync('index.html','utf8'));
-console.log(buf.toString('utf8', 0, len));
+var app = express.createServer(express.logger());
 
-//console.log(buf.toString
+app.get('/', function(request, response) {
+  response.send(texte);
+});
 
-
-
-//var app = express.createServer(express.logger());
-
-//app.get('/', function(request, response) {
-//  response.send(len);
-//});
-
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
