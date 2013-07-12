4ef1569b21278738733f8f69d55a547b801c31b4
Fred <fledinahet@gmail.com>
2013-07-12 17:02:43 +0000

    SImple version of web.js

--- web.js
+++ web.js
@@ -1,24 +1,26 @@
-var express = require('express');
+//var express = require('express');
 
 var fs = require('fs');
 
 buf = new Buffer(256);
-
-fs.readFileSync('/index.html', function (err, data) {
-  if (err) throw err;
-  buf.write(data);
-  len = buf.toString();
-});
+len = buf.write(fs.readFileSync('index.html','utf8'));
 
 
 
-var app = express.createServer(express.logger());
+//console.log(fs.readFileSync('index.html','utf8'));
+console.log(buf.toString('utf8', 0, len));
 
-app.get('/', function(request, response) {
-  response.send(len);
-});
+//console.log(buf.toString
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+
+//var app = express.createServer(express.logger());
+
+//app.get('/', function(request, response) {
+//  response.send(len);
+//});
+
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
