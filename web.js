0d73b61ca703741320c25e733d554f1719e3dd52
Fred <fledinahet@gmail.com>
2013-07-12 15:38:24 +0000

    tried change web.js

--- web.js
+++ web.js
@@ -1,9 +1,21 @@
 var express = require('express');
 
+var fs = require('fs');
+
+buf = new Buffer(256);
+
+fs.readFileSync('/index.html', function (err, data) {
+  if (err) throw err;
+  buf.write(data);
+  len = buf.toString();
+});
+
+
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(len);
 });
 
 var port = process.env.PORT || 5000;
