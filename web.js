26b8b85bbf3c69161ec853c00085673af50b39e4
Fred <fledinahet@gmail.com>
2013-07-29 14:47:00 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -1,18 +1,13 @@
 var express = require('express');
-
 var fs = require('fs');
-
-buf = new Buffer(20000);
-len = buf.write(fs.readFileSync('index.html','utf8'));
-
-texte = buf.toString('utf8', 0, len);
-console.log(texte);
+var htmlfile = "index.html";
 
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(texte);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
 var port = process.env.PORT || 8080;
